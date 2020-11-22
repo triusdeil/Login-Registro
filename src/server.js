7ef249a22e05592707a8842bfa9af9de5a320085
Luis Ascanio <triusdeil@gmail.com>
2020-11-21 23:57:19 -0600

    creando los modulos

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
+//modulo express => permite crear el servidor
 const express = require('express')
 const app = express()
+//modulo express-session
+const session = require('express-session')
+//modulo path => permite manejar las rutas(las carpetas)
+const path =  require('path')
+//modulo mongoose para conectar a la base de datos
+const mongoose = require('mongoose')
+//modulo passport nos permite configurar la manera en que nos vamos a autenticar
+const passport = require('passport')
+//modulo connect-flash
+const flash = require('connect-flash')
+//modulo morgan definir los metodos http que llegan al servidor y verlos por consola
+const morgan = require('morgan')
+//modulo cookie para poder administrar las cookies
+const cookieParser = require('cookie-parser')
+//modulo body-parser converti el cuerpo de la informacion que llegue del navegador al servidor
+const bodyParser = require('body-parser')
+
 
 //dividir el servidor
-
 //settings
 app.set('port',process.env.PORT || 3000)
 //middlewares
